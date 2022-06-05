8ea98c69aacbee54fe29cafa10a015e8d10f9701
Crimson Snow <juri.wiechmann@gmx.net>
2022-06-05 16:20:50 +0200

    first review

---
+++ Assets/Project/ECS_Stack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Project
+{
+    public class ECS_Stack
+    {
+        public bool IsEmpty => _head == null;
+        public NodeData Head => _head.data;
+        
+        private ECS_Node _head;
+
+        public void Push(NodeData data)
+        {
+            ECS_Node node = new ECS_Node(data);
+            if (_head != null)
+                node.next = _head;
+            else
+            {
+                node.next = null;
+            }
+            _head = node;
+        }
+
+        public NodeData Pop()
+        {
+            if(IsEmpty)
+                throw new Exception("the stack is empty");
+
+            ECS_Node node = new ECS_Node(_head.data);
+            _head = _head.next;
+            return node.data;
+        }
+
+        public List<NodeData> Traverse()
+        {
+            var result = new List<NodeData>();
+
+            var crrNode = _head;
+
+            while (crrNode != null)
+            {
+                result.Add(crrNode.data);
+                crrNode = crrNode.next;
+            }
+
+            return result;
+        }
+        
+        
+        private class ECS_Node
+        {
+            public ECS_Node next;
+            public NodeData data;
+
+            public ECS_Node(NodeData data)
+            {
+                this.data = data;
+            }
+        }
+    }
+}
---
+++ Assets/Project/UseStackSystem.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using Unity.Collections;
+using Unity.Entities;
+using UnityEngine;
+
+namespace Project
+{
+    [AlwaysUpdateSystem]
+    public partial class UseStackSystem : SystemBase
+    {
+        private const int StackSize = 20;
+        private const int MaxEntitiesPerChunk = 2040;
+
+        private EndSimulationEntityCommandBufferSystem _ecbs;
+        private EntityCommandBuffer.ParallelWriter _ecb;
+        
+        private readonly ECS_Stack _stack = new ECS_Stack();
+        private IEnumerator _textStack;
+
+        protected override void OnCreate()
+        {
+            _ecbs = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+
+            _textStack = TestStack().GetEnumerator();
+        }
+
+        protected override void OnUpdate()
+        {
+            _textStack.MoveNext();
+        }
+
+        private void FillStack()
+        {
+            for (var i = 0; i != StackSize - 1; i++)
+            {
+                var component = new NodeData { index = i};
+                _stack.Push(component);
+                for (var j = 0; j != MaxEntitiesPerChunk; j++)
+                {
+                    var entity = _ecb.CreateEntity(j);
+                    _ecb.AddSharedComponent(j, entity, component);
+                }
+            }
+        }
+
+        private void SplitHead()
+        {
+            var lastNodeData = new NodeData { index = StackSize - 1};
+            
+            var query = GetEntityQuery(ComponentType.ReadOnly<NodeData>());
+            query.SetSharedComponentFilter(_stack.Head);
+            
+            _stack.Push(lastNodeData);
+            
+            var entities = query.ToEntityArray(Allocator.Temp);
+            for (var i = 0; i != entities.Length / 2; i++)
+                _ecb.SetSharedComponent(i, entities[i], lastNodeData);
+            
+        }
+        
+        private void EmptyStack()
+        {
+            while (!_stack.IsEmpty)
+            {
+                var data = _stack.Pop();
+                Entities
+                    .WithoutBurst()
+                    .WithSharedComponentFilter(data)
+                    .ForEach((Entity e, int entityInQueryIndex) =>
+                    {
+                        _ecb.DestroyEntity(entityInQueryIndex, e);
+                    }).Run();
+            }
+        }
+
+        private IEnumerable TestStack()
+        {
+            _ecb = _ecbs.CreateCommandBuffer().AsParallelWriter();
+            FillStack();
+            yield return null;
+            PrintStack();
+            _ecb = _ecbs.CreateCommandBuffer().AsParallelWriter();
+            SplitHead();
+            yield return null;
+            PrintStack();
+            /*
+            _ecb = _ecbs.CreateCommandBuffer().AsParallelWriter();
+            EmptyStack();
+            yield return null;
+            PrintStack();
+            */
+        }
+
+        private void PrintStack()
+        {
+            var data = _stack.Traverse();
+            var query = GetEntityQuery(ComponentType.ReadOnly<NodeData>());
+            Debug.Log($"The Stack has {data.Count} nodes");
+            foreach (var nodeData in data)
+            {
+                query.SetSharedComponentFilter(nodeData);
+                Debug.Log($"Node with ID = {nodeData.index} has {query.CalculateEntityCount()} Entities");
+                query.ResetFilter();
+            }
+            Debug.Log("---------------------------------");
+        }
+    }
+}
